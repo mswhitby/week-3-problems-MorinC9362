d1af2dcf1ffad346ce0bfd8322979b11c1ce0fa2
MorinC9362 <58002196+MorinC9362@users.noreply.github.com>
2022-08-30 12:41:07 -0500

    Update adding_numbers.py

--- adding_numbers.py
+++ adding_numbers.py
@@ -1,1 +1,15 @@
+inp1 = str(input("num1: "))
+inp2 = str(input("num2: "))
+  #receive input string vals
+def add(number1, number2):
+    number1 = int(number1)
+    number2 = int(number2)
+    #convert to int
+    return number1 + number2
+  #add
+result = add(inp1, inp2)
+result = str(result)
+  #back to int then print
+print(result)
+
 
