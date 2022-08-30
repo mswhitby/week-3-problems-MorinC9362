4f8316d9a4b4bebafcb6fe94a596715e8c6130bf
MorinC9362 <58002196+MorinC9362@users.noreply.github.com>
2022-08-30 13:08:56 -0500

    Update adding_numbers.py

--- adding_numbers.py
+++ adding_numbers.py
@@ -1,15 +1,19 @@
 inp1 = str(input("num1: "))
 inp2 = str(input("num2: "))
-  #receive input string vals
+x = 4
+if((inp1 and inp2) <= '9')  and ((inp1 and inp2) >= '0'):
+    x = 0
+    
+if x == 4:
+    print("invalid input")
+    exit()
 def add(number1, number2):
     number1 = int(number1)
     number2 = int(number2)
-    #convert to int
+
     return number1 + number2
-  #add
 result = add(inp1, inp2)
 result = str(result)
-  #back to int then print
 print(result)
 
 
